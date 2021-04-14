dd458e88099b80ab732f9346bdc7d8c5b66ce58c
Nadia <nadiyaklokova@gmail.com>
2021-04-14 22:43:13 +0200

    creamos NavLink y <a> en SASS

--- react-movie/src/components/Header/Header.jsx
+++ react-movie/src/components/Header/Header.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './Header.scss'
 
 const Header = props => {
     return(<header>
-        <span onclick="getPopularMovies();backgroundAzul()">Populares</span>
-        <span onclick="getComingMovies();backgroundAmarillo()">Proximos estrenos</span>
-        <span onclick="getComingMovies();backgroundAmarillo()">Más votadas</span>
+        <NavLink to='/popular'>
+            <span className='populares' onclick="getPopularMovies();backgroundAzul()">Populares</span>
+        </NavLink>
+
+        <NavLink to='/upcoming'>
+            <span className='upcoming'onclick="getComingMovies();backgroundAmarillo()">Proximos estrenos</span>
+        </NavLink>
+        
+        <NavLink to='/top_rated'>
+            <span className='top_rated'onclick="getComingMovies();backgroundAmarillo()">Más votadas</span>
+        </NavLink>
+        
     </header>
     )
 }
