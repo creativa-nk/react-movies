41e5040166b07eebac154e62b929151de91590f7
Nadia <nadiyaklokova@gmail.com>
2021-04-15 22:39:30 +0200

    SASS movie hecho

--- react-movie/src/components/Movie/Movie.jsx
+++ react-movie/src/components/Movie/Movie.jsx
@@ -2,10 +2,19 @@ import React from 'react';
 import '../Movie/Movie.scss'
 
 const Movie = ({movie}) =>{
-    return <div className="movie">
-        <h4>{movie.title}</h4>
-        <img src={"http://image.tmdb.org/t/p/w185"+movie.poster_path} alt=""/>
-        <span>{movie.vote_average}</span>
-    </div>
-}
+    return(
+        <div className="movie" onclick='getMovieDetailed(${movie.id})'>
+            <div className='titulo'>
+                 <h2>{movie.title}</h2>
+            </div>
+            <div className='imgMovie'>  
+                <img src={"http://image.tmdb.org/t/p/w185"+movie.poster_path} alt="imagen de la pelicula"/>    
+                    <div className='average'>
+                         <p>{movie.vote_average}</p>
+                     </div>
+            </div>
+        </div>    
+)};
+    
+  
 export default Movie;
