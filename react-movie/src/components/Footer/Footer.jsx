a3516f0188ec0e8c73ce82c1e81a466254c86e46
Nadia <nadiyaklokova@gmail.com>
2021-04-17 12:33:05 +0200

    creado Footer

---
+++ react-movie/src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Footer.scss';
+import '../../img/multiAmarillo.jpg'
+
+const Footer = props => {
+    return(
+        <footer>
+            <p>Made by ☺ creativa-nk</p>
+        </footer>
+    )
+}
+export default Footer;
