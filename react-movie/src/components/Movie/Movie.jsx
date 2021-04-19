ce39ba14efd01a032647eb03f2121aa7fcb68662
Nadia <nadiyaklokova@gmail.com>
2021-04-20 00:38:24 +0200

    vote_average de colores

--- react-movie/src/components/Movie/Movie.jsx
+++ react-movie/src/components/Movie/Movie.jsx
@@ -1,30 +1,46 @@
 import React from 'react';
 import '../Movie/Movie.scss'
 
+const setVoteClass = (vote) =>{
+    if(vote >= 8){
+        return 'green'
+    }else if(vote >=6 ){
+        return 'orange'
+    }else{
+        return 'red';
+    }
+}
+
+const IMG_API = "http://image.tmdb.org/t/p/w185";
 const Movie = ({movie}) =>{
   
     return(
-        <div className="movie" /* onclick='getMovieDetailed(${movie.id})' */>
+        <div className="movie">
            
-                 <h2>{movie.title}</h2>
+            <div className='movieTitle'>
+                  <div >{movie.title}</div>
+            </div>
             
             <div className='imgMovie'>  
-                <img src={"http://image.tmdb.org/t/p/w185"+ movie.poster_path} alt="imagen de la pelicula"/>    
+                <img src={IMG_API + movie.poster_path} alt="imagen de la pelicula"/>    
                     <div className='average'>
-                         <p>{movie.vote_average}</p>
+                         <p className={
+                             `tag ${setVoteClass(movie.vote_average)}`
+                             }>
+                                 {movie.vote_average}</p>
                      </div>
             </div>
             <div className='moreDetails'>
                             <h3 className='originalTitle'>{movie.original_title}</h3>
-                            <span>({movie.release_date})</span>
-                          {/*   <p className='genres'>{movie.genres.map(genre => genres.push(genre.name)).join(', ')}</p> */}                            
+                            <span>({movie.release_date})</span>                          
                             <span>{movie.runtime} min</span>
                            <h4> Sinopsis:</h4>{movie.overview}
                             
             </div>
 
-        </div>    
+        </div> 
+       
 )};
-    
+console.log(Movie)       
   
 export default Movie;
